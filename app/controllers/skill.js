72f3f5e2bd464da7173e91bff2c1e0c73546556e
nyugoh <nyugoh@gmail.com>
2018-05-18 04:28:16 +0300

    Added list skill api

--- app/controllers/skill.js
+++ app/controllers/skill.js
@@ -3,16 +3,9 @@ const router = express.Router();
 import Skill from '../models/Skill';
 
 module.exports = (app) => {
-  app.use('/api/v1/skill', router);
+  app.use('/api/v1/skills', router);
 };
 
-router.get('/', (req, res) =>{
-  Skill.find({user: req.user._id}).sort({'createdAt': -1}).then(skills=>{
-    res.render('skills/index', { title: 'Your skills', user: req.user, skills:skills});
-  }).catch(error=> {
-    res.status(404).json({message: error.message});
-  });
-});
 
 router.post('/add', (req, res) =>{
   if(req.body) {
@@ -22,14 +15,14 @@ router.post('/add', (req, res) =>{
       res.json({status: 'OK', skill});
     }).catch(error =>{
       res.status(404).json({message: error.message});
-    })
+    });
   } else {
     res.status(404).json({message: "Invalid skill."});
   }
 });
 
-router.get('/list', (req, res) =>{
-  Skill.find({user: req.user._id}).sort({'createdAt': -1}).then(skills=>{
+router.get('/get', (req, res) =>{
+  Skill.find({}).sort({'createdAt': -1}).then(skills=>{
     res.json({skills});
   }).catch(error=>{
     res.status(404).json({message: error.message});
@@ -47,7 +40,7 @@ router.post('/remove', (req, res) =>{
 
 router.get('/:skillSubject', (req, res, next) =>{
   Skill.findOne({$or:[{subject: req.params.skillSubject.toLowerCase()}, {subject: req.params.skillSubject.toUpperCase()}]}).then(skill =>{
-    res.render('skills/skill', {title: skill.subject,user: req.user, skill});
+    res.json({title: skill.subject,user: req.user, skill});
   }).catch(error =>{
     next(error);
   });
